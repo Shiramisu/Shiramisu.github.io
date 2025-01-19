70a986944edf81f0befd7aea96595dbbb38470d1
Syles <166589073+Shiramisu@users.noreply.github.com>
2025-01-19 22:20:45 +0100

    delete

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    document.querySelectorAll('.header, .glowing-card, footer').forEach(element => {
-        element.classList.toggle('dark-mode');
-    });
-}
